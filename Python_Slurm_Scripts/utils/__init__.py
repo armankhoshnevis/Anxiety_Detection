874026bfeb289fd4918e7f259f761c5d44b9509a
armankhoshnevis <khoshne1@msu.edu>
2026-02-09 16:39:17 -0500

    Correct the naming

--- Python_Slurm_Scripts/utils/__init__.py
+++ Python_Slurm_Scripts/utils/__init__.py
@@ -3,7 +3,7 @@
 from .data_loader import get_training_data, make_data_grid
 from .preprocessing import lof_outlier_removal, mi_score_func
 from .model_factory import build_pipeline, param_space
-from .post_preocessing import save_results, compute_fold_shap, plot_shap_summary
+from .post_processing import save_results, compute_fold_shap, plot_shap_summary
 
 __all__ = [
     "get_training_data",

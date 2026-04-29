0107eca871cece315faef8e2b743abb11b68d4f5
armankhoshnevis <khoshne1@msu.edu>
2026-04-29 15:06:44 -0400

    Refactor lof_outlier_removal function and remove unused mi_score_func

--- Python_Slurm_Scripts/utils/preprocessing.py
+++ Python_Slurm_Scripts/utils/preprocessing.py
@@ -4,8 +4,7 @@ from sklearn.feature_selection import mutual_info_classif
 
 # Outlier removal using Local Outlier Factor
 def lof_outlier_removal(X, y, n_neighbors=20, contamination=0.05, algorithm='auto', metric='manhattan'):
-    """
-    Removes outliers from the dataset using Local Outlier Factor (LOF).
+    """Removes outliers from the dataset using Local Outlier Factor (LOF).
     Args:
         X (pandas DataFrame): Feature matrix.
         y (pandas Series): Target vector.
@@ -17,32 +16,15 @@ def lof_outlier_removal(X, y, n_neighbors=20, contamination=0.05, algorithm='aut
     Returns:
         tuple: Filtered feature matrix and target vector as numpy arrays.
     """
-
     lof = LocalOutlierFactor(
         n_neighbors=n_neighbors,
         contamination=contamination,
         algorithm=algorithm,
         metric=metric,
         leaf_size=30,
-        novelty=False
-    )
+        novelty=False)
     
     y_pred = lof.fit_predict(X)
     mask_inliers = y_pred == 1
 
-    return X[mask_inliers], y[mask_inliers]
-
-def mi_score_func(X, y, random_state=42, n_neighbors=3):
-    """
-    Computes mutual information scores between feature and target for feature selection step.
-    Args:
-        X (pandas DataFrame): Feature matrix.
-        y (pandas Series): Target vector.
-        random_state (int): Random state for reproducibility. Defaults to 42.
-        n_neighbors (int): Number of neighbors to use for MI calculation. Defaults to 3.
-
-    Returns:
-        scores (numpy array): Mutual information scores for each feature.
-    """
-    scores = mutual_info_classif(X, y, random_state=random_state, n_neighbors=n_neighbors)
-    return scores
+    return X.iloc[mask_inliers], y.iloc[mask_inliers]

9c7923c00318c3474a0059c5dc35341dd7d04627
armankhoshnevis <khoshne1@msu.edu>
2026-06-06 21:09:43 -0400

    Refactor model_factory to support the regression task

--- scripts/utils/model_factory.py
+++ scripts/utils/model_factory.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import PowerTransformer
 from imblearn import FunctionSampler
 
 from sklearn.feature_selection import RFE
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 from imblearn.over_sampling import SMOTE, SMOTENC
 
@@ -18,14 +18,17 @@ from sklearn.compose import ColumnTransformer
 
 from imblearn.pipeline import Pipeline as ImbPipeline
 
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC, SVR
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import (
+    RandomForestClassifier, RandomForestRegressor,
+    GradientBoostingClassifier, GradientBoostingRegressor,
+)
+from xgboost import XGBClassifier, XGBRegressor
+from lightgbm import LGBMClassifier, LGBMRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
 
 # Build the machine learning pipeline
 def build_pipeline(config, num_cols, cat_cols, memory=None):
@@ -41,6 +44,8 @@ def build_pipeline(config, num_cols, cat_cols, memory=None):
     Returns:
         pipeline (ImbPipeline): An imbalanced-learn pipeline with the specified steps.
     """
+    is_classification = config["prediction_task"] == "classification-binary"
+    
     yj_pt = PowerTransformer(method="yeo-johnson", standardize=True)
     if config["feature_set"] == "eGeMAPS_Demographics":
         preprocessor = ColumnTransformer(
@@ -53,10 +58,17 @@ def build_pipeline(config, num_cols, cat_cols, memory=None):
         )
         
         cat_indices = list(range(len(num_cols), len(num_cols) + len(cat_cols)))
-        oversampler = SMOTENC(categorical_features=cat_indices, random_state=42)
+        oversampler = (
+            SMOTENC(categorical_features=cat_indices, random_state=42)
+            if is_classification else "passthrough"
+        )
+    
     else:
         preprocessor = yj_pt
-        oversampler = SMOTE(random_state=42)
+        oversampler = (
+            SMOTE(random_state=42) 
+            if is_classification else "passthrough"
+        )
     
     lof_sampler = FunctionSampler(
         func=lof_outlier_removal,
@@ -70,64 +82,83 @@ def build_pipeline(config, num_cols, cat_cols, memory=None):
     )
     
     if config["feature_selector_method"] == "rfe":
-        feature_selector = RFE(estimator=RandomForestClassifier(n_estimators=25, random_state=42), step = 0.1)
+        feature_selector = (
+            RFE(estimator=RandomForestClassifier(n_estimators=25, random_state=42), step = 0.1)
+            if is_classification 
+            else RFE(estimator=RandomForestRegressor(n_estimators=25, random_state=42), step = 0.1)
+        )
     
     elif config["feature_selector_method"] == "passthrough":
         feature_selector = "passthrough"
 
     model_name = config["model_name"]
     if model_name == "SVC":
-        clf = SVC(probability=True, random_state=42)
+        model = (
+            SVC(probability=True, random_state=42)
+            if is_classification
+            else SVR()
+        )
     
     elif model_name == "DT":
-        clf = DecisionTreeClassifier(random_state=42)
+        model = (
+            DecisionTreeClassifier(random_state=42)
+            if is_classification
+            else DecisionTreeRegressor(random_state=42)
+        )
     
     elif model_name == "RF":
-        clf = RandomForestClassifier(
-            class_weight=None,
-            n_jobs=1,
-            random_state=42,
-            )
+        model = (
+            RandomForestClassifier(class_weight=None, n_jobs=1, random_state=42)
+            if is_classification
+            else RandomForestRegressor(n_jobs=1, random_state=42)
+        )
     
     elif model_name == "GB":
-        clf = GradientBoostingClassifier(
-            n_estimators=1500,
-            n_iter_no_change=10,
-            tol=1e-4,
-            validation_fraction=0.10,
-            random_state=42,
+        model = (
+            GradientBoostingClassifier(n_estimators=1500, n_iter_no_change=10, tol=1e-4,
+                                       validation_fraction=0.10, random_state=42)
+            if is_classification
+            else GradientBoostingRegressor(n_estimators=1500, n_iter_no_change=10, tol=1e-4,
+                                           validation_fraction=0.10, random_state=42)
         )
     
     elif model_name == "XGB":
-        clf = XGBClassifier(
-            booster="gbtree",
-            tree_method="hist",
-            n_jobs=1,
-            verbosity=3,
-            random_state=42
+        model = (
+            XGBClassifier(booster="gbtree", tree_method="hist", n_jobs=1,
+                          verbosity=3, random_state=42)
+            if is_classification
+            else XGBRegressor(booster="gbtree", tree_method="hist", n_jobs=1,
+                              verbosity=3, random_state=42)
         )
     
     elif model_name == "LGBM":
-        clf = LGBMClassifier(
-            boosting_type="gbdt",
-            n_jobs=1,
-            random_state=42,
+        model = (
+            LGBMClassifier(boosting_type="gbdt", n_jobs=1, random_state=42)
+            if is_classification
+            else LGBMRegressor(boosting_type="gbdt", n_jobs=1, random_state=42)
         )
     
     elif model_name == "MLP":
-        clf = MLPClassifier(
-            random_state=42,
-            max_iter=2000,
-            early_stopping=True,
-            n_iter_no_change=15,
-            validation_fraction=0.15
+        model = (
+            MLPClassifier(random_state=42, max_iter=2000, early_stopping=True,
+                            n_iter_no_change=15, validation_fraction=0.15)
+            if is_classification
+            else MLPRegressor(random_state=42, max_iter=2000, early_stopping=True,
+                              n_iter_no_change=15, validation_fraction=0.15)
         )
     
     elif model_name == "NB":
-        clf = GaussianNB()
+        if is_classification:
+            model = GaussianNB()
+        else:
+            raise ValueError("Naive Bayes is not supported for regression tasks.")
     
     elif model_name == "KNN":
-        clf = KNeighborsClassifier()
+        model = (
+            KNeighborsClassifier()
+            if is_classification
+            else KNeighborsRegressor()
+        )
     
     else:
         raise ValueError(f"Unsupported model_name: {model_name}")
@@ -137,7 +168,7 @@ def build_pipeline(config, num_cols, cat_cols, memory=None):
         ("outlier_removal", lof_sampler),
         ("oversampler", oversampler),
         ("feature_selector", feature_selector),
-        ("classifier", clf),
+        ("model", model),
     ]
     
     pipeline = ImbPipeline(steps=steps, memory=memory).set_output(transform="pandas")
@@ -163,78 +194,69 @@ def param_space(config):
             "feature_selector__n_features_to_select": uniform(0.1, 0.9),  # [0.1, 1.0]
         })
     
+    if config["prediction_task"] == "classification-binary":
+        param_distributions["oversampler__k_neighbors"] = randint(3, 8)  # [3, 7]
+    
     if model_name == "SVC":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__C": loguniform(1e-3, 1e6),
-            "classifier__gamma": loguniform(1e-6, 1e2),
-            "classifier__kernel": ["rbf"],
+            "model__C": loguniform(1e-3, 1e6),
+            "model__gamma": loguniform(1e-6, 1e2),
+            "model__kernel": ["rbf"],
         })
-        return param_distributions
     
     elif model_name == "DT":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__max_depth": randint(3, 21),  # [3, 20]
-            "classifier__max_features": ["sqrt", "log2", 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-            "classifier__min_samples_split": uniform(0.05, 0.35),  # Fraction [0.05, 0.4]
-            "classifier__min_samples_leaf": uniform(0.01, 0.09),  # Fraction [0.01, 0.1]
-            "classifier__ccp_alpha": loguniform(1e-6, 1e-1),
+            "model__max_depth": randint(3, 21),  # [3, 20]
+            "model__max_features": ["sqrt", "log2", 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+            "model__min_samples_split": uniform(0.05, 0.35),  # Fraction [0.05, 0.4]
+            "model__min_samples_leaf": uniform(0.01, 0.09),  # Fraction [0.01, 0.1]
+            "model__ccp_alpha": loguniform(1e-6, 1e-1),
         })
-        return param_distributions
     
     elif model_name == "RF":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__n_estimators": randint(200, 1001),  # [200, 1000]
-            "classifier__max_depth": randint(3, 21),  # [3, 20]
-            "classifier__max_features": ["sqrt", "log2", 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-            "classifier__min_samples_split": uniform(0.05, 0.45),  # [0.05, 0.5]
-            "classifier__min_samples_leaf": uniform(0.01, 0.19),  # [0.01, 0.2]
-            "classifier__ccp_alpha": loguniform(1e-6, 1e-1),
+            "model__n_estimators": randint(200, 1001),  # [200, 1000]
+            "model__max_depth": randint(3, 21),  # [3, 20]
+            "model__max_features": ["sqrt", "log2", 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+            "model__min_samples_split": uniform(0.05, 0.45),  # [0.05, 0.5]
+            "model__min_samples_leaf": uniform(0.01, 0.19),  # [0.01, 0.2]
+            "model__ccp_alpha": loguniform(1e-6, 1e-1),
         })
-        return param_distributions
     
     elif model_name == "GB":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__learning_rate": loguniform(5e-3, 5e-1),
-            "classifier__max_depth": randint(3, 8),  # [3, 7]
-            "classifier__max_features": ["sqrt", "log2", 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-            "classifier__min_samples_split": uniform(0.05, 0.30),  # [0.05, 0.35]
-            "classifier__min_samples_leaf": uniform(0.01, 0.09),  # [0.01, 0.1]
-            "classifier__subsample": uniform(0.75, 0.25),  # [0.75, 1.0]
+            "model__learning_rate": loguniform(5e-3, 5e-1),
+            "model__max_depth": randint(3, 8),  # [3, 7]
+            "model__max_features": ["sqrt", "log2", 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+            "model__min_samples_split": uniform(0.05, 0.30),  # [0.05, 0.35]
+            "model__min_samples_leaf": uniform(0.01, 0.09),  # [0.01, 0.1]
+            "model__subsample": uniform(0.75, 0.25),  # [0.75, 1.0]
         })
-        return param_distributions
     
     elif model_name == "XGB":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__n_estimators": randint(200, 1001),  # [200, 1000]
-            "classifier__learning_rate": loguniform(1e-3, 3e-1),  # [0.001, 0.3]
-            "classifier__min_child_weight": uniform(1, 7),  # [1, 8]
-            "classifier__max_depth": randint(3, 11),  # [3, 10]
-            "classifier__gamma": uniform(0, 1),  # [0, 1]
-            "classifier__subsample": uniform(0.6, 0.4),  # [0.6, 1.0]
-            "classifier__colsample_bytree": uniform(0.6, 0.4),  # [0.6, 1.0]
-            "classifier__reg_lambda": loguniform(1e-3, 1e1),  # [0.001, 10]
+            "model__n_estimators": randint(200, 1001),  # [200, 1000]
+            "model__learning_rate": loguniform(1e-3, 3e-1),  # [0.001, 0.3]
+            "model__min_child_weight": uniform(1, 7),  # [1, 8]
+            "model__max_depth": randint(3, 11),  # [3, 10]
+            "model__gamma": uniform(0, 1),  # [0, 1]
+            "model__subsample": uniform(0.6, 0.4),  # [0.6, 1.0]
+            "model__colsample_bytree": uniform(0.6, 0.4),  # [0.6, 1.0]
+            "model__reg_lambda": loguniform(1e-3, 1e1),  # [0.001, 10]
         })
-        return param_distributions
     
     elif model_name == "LGBM":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__num_leaves": randint(24, 91),  # [24, 90]
-            "classifier__min_child_samples": randint(15, 101),  # [15, 100]
-            "classifier__max_depth": randint(3, 11),  # [3, 10]
-            "classifier__min_split_gain": loguniform(1e-3, 1e1),  # [0.001, 10]
-            "classifier__n_estimators": randint(200, 1001),  # [200, 1000]
-            "classifier__learning_rate": loguniform(1e-3, 3e-1),  # [0.001, 0.3]
-            "classifier__subsample": uniform(0.6, 0.4),  # [0.6, 1.0]
-            "classifier__colsample_bytree": uniform(0.6, 0.4),  # [0.6, 1.0]
-            "classifier__reg_lambda": loguniform(1e-3, 1e1),  # [0.001, 10]
+            "model__num_leaves": randint(24, 91),  # [24, 90]
+            "model__min_child_samples": randint(15, 101),  # [15, 100]
+            "model__max_depth": randint(3, 11),  # [3, 10]
+            "model__min_split_gain": loguniform(1e-3, 1e1),  # [0.001, 10]
+            "model__n_estimators": randint(200, 1001),  # [200, 1000]
+            "model__learning_rate": loguniform(1e-3, 3e-1),  # [0.001, 0.3]
+            "model__subsample": uniform(0.6, 0.4),  # [0.6, 1.0]
+            "model__colsample_bytree": uniform(0.6, 0.4),  # [0.6, 1.0]
+            "model__reg_lambda": loguniform(1e-3, 1e1),  # [0.001, 10]
         })
-        return param_distributions
     
     elif model_name == "MLP":
         hidden_layers_counts = [3, 4, 5, 6]
@@ -246,32 +268,28 @@ def param_space(config):
         ]
 
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__hidden_layer_sizes": hidden_layer_sizes,
-            "classifier__learning_rate_init": loguniform(1e-5, 1e-2),  # [0.00001, 0.01]
-            "classifier__batch_size": [16, 32, 64, 128, "auto"],
-            "classifier__activation": ["relu", "tanh", "logistic"],
-            "classifier__solver": ["adam", "sgd", "lbfgs"],
-            "classifier__alpha": loguniform(1e-5, 1e-2),  # [0.00001, 0.01]
+            "model__hidden_layer_sizes": hidden_layer_sizes,
+            "model__learning_rate_init": loguniform(1e-5, 1e-2),  # [0.00001, 0.01]
+            "model__batch_size": [16, 32, 64, 128, "auto"],
+            "model__activation": ["relu", "tanh", "logistic"],
+            "model__solver": ["adam", "sgd", "lbfgs"],
+            "model__alpha": loguniform(1e-5, 1e-2),  # [0.00001, 0.01]
         })
-        return param_distributions
     
     elif model_name == "NB":
         param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__var_smoothing": loguniform(1e-11, 1e-7),  # [1e-11, 1e-7]
+            "model__var_smoothing": loguniform(1e-11, 1e-7),  # [1e-11, 1e-7]
         })
-        return param_distributions
     
     elif model_name == "KNN":
-        param_distributions.update({
-            "oversampler__k_neighbors": randint(3, 8),  # [3, 7]
-            "classifier__n_neighbors": list(range(3, 22, 2)),  # [3, 5, 7, ..., 21]
-            "classifier__weights": ["uniform", "distance"],
-            "classifier__algorithm": ["ball_tree", "kd_tree", "brute"],
-            "classifier__p": [1, 2],
+        param_distributions.update({            
+            "model__n_neighbors": list(range(3, 22, 2)),  # [3, 5, 7, ..., 21]
+            "model__weights": ["uniform", "distance"],
+            "model__algorithm": ["ball_tree", "kd_tree", "brute"],
+            "model__p": [1, 2],
         })
-        return param_distributions
     
     else:
         raise ValueError(f"Unsupported model_name: {model_name}")
+    
+    return param_distributions
